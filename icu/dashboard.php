17ae2b3132663251bfe4a568364abd7e95fdd6ae
Tarun Kumar Dixit <tarundixit0310@gmail.com>
2020-09-13 16:19:04 +0530

    Dashboard table

--- icu/dashboard.php
+++ icu/dashboard.php
@@ -1,12 +1,6 @@
 <?php include "dash_common.php"; ?>
 <?php
-// $id = $_SESSION["UID"];
-// $name = e_d('d',$_SESSION["fullName"]);
-// $email = e_d('d',$_SESSION["emailAddress"]);
-// $phone = e_d('d',$_SESSION["phoneNumber"]);
-// $departmentID = $_SESSION["departmentID"];
-// $qualificationID = $_SESSION["qualificationID"];
-// $hospitalID = $_SESSION["hospitalID"];
+$patients = getThis("SELECT `id`, `patientID`, `doctorID`, `bedID` from `ipdlog` WHERE `roomID`='$id'");
 
 ?>
 <!doctype html>
@@ -28,16 +22,78 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            
-        </div>
+
+        <?php
+        if (sizeof($patients) == 0) {
+        ?>
+            <h4>There is no patient in this ICU</h4>
+        <?php
+        } else {
+        ?>
+
+            <div class="col-md-12">
+                <div class="main-card mb-3 card" style="overflow-x:scroll;">
+                    <div class="card-body">
+                        <h5 class="card-title">ICU Details</h5>
+                        <table class="mb-0 table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Bed Number</th>
+                                    <th>Patient Name</th>
+                                    <th>Lead Doctor</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                for ($i = 0; $i < sizeof($patients); $i++) {
+
+                                ?>
+                                    <tr>
+                                        <td>
+                                            <?php
+                                            $x = $patients[$i]['bedID'];
+                                            $bedNumber = getThis("SELECT `bedNumber` FROM `beds` WHERE `id`='$x' AND `enabled`='1' ");
+                                            $bedName = $bedNumber[0]['bedNumber'];
+                                            echo e_d('d', $bedName);
+                                            ?>
+
+                                        </td>
+                                        <td>
+                                            <?php
+                                            $x = $patients[$i]['patientID'];
+                                            $name = getThis("SELECT `id`, `fullName` FROM `patients` WHERE `id` = '$x'");
+                                            $fullName = $name[0]['fullName'];
+                                            echo e_d('d', $fullName);
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <?php
+                                            $x = $patients[$i]['doctorID'];
+                                            $docName = getThis("SELECT `fullName` FROM `doctors` WHERE `id` = '$x'");
+                                            $docName = $docName[0]['fullName'];
+                                            echo e_d('d', $docName);
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <a href="test.php?id=<?php echo e_d('e', $patients[$i]['id']); ?>" class="btn btn-block btn-primary">View Details</a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
     </div>
 </div>
-</div>
-</div>
 
 
-
+<?php
+        } ?>
 
 </body>
 
