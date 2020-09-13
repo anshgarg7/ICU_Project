d941c356eedda105b7771716a035a6d98ba964a9
Ansh Garg <anshgarg7@gmail.com>
2020-09-13 15:31:17 +0530

    form fix

--- icu/functionality/iculoginact.php
+++ icu/functionality/iculoginact.php
@@ -2,12 +2,9 @@
 include "../../assets/fxn.php";
 $user = e_d('e', $_POST["username"]);
 $pass = e_d('e', $_POST["password"]);
-
-$login = getThis("SELECT `id`, `hospitalId`, `roomName`, `roomDescription`, `roomLocation`, `roomUsername`, `roomPassword`, `totalBeds`, `generatedAt`, `enabled` FROM `rooms` WHERE `roomUsername`='$user' AND `roomPassword`='$pass' AND `enabled`=1");
-//$temp_login = $con->query("SELECT doctors.`id` AS id,`hospitalID`,`qualificationID`,`departmentID`, `fullName`, `phoneNumber`, `emailAddress` FROM `doctors` WHERE `username`='$user'   AND `password`='$pass' AND doctors.`enabled`=1");
+$login = getThis("SELECT `id`, `hospitalId`, `roomName`, `roomDescription`, `roomLocation`, `roomUsername`, `roomPassword`, `totalBeds`, `generatedAt`, `enabled` FROM `rooms` WHERE `roomUsername`='$user' AND `roomPassword`='$pass' AND `enabled`='1'");
 if ($login) {
 	$login = $login[0];
-
 	if ($login["id"] != null) {
 		$id = $login["id"];
 		$_SESSION["UID"] = $login["id"];
