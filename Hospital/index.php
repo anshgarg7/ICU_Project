fe55d74c02cadc819e0b65ccb7054e7a55fd2ba9
Ansh Garg <anshgarg7@gmail.com>
2020-09-15 17:16:11 +0530

    login pages fix

--- Hospital/index.php
+++ Hospital/index.php
@@ -1,132 +1,54 @@
-<?php
-include "../assets/fxn.php";
-if(isset($_SESSION["UID"])!=null){
-	?>
-	<script type="text/javascript">
-		window.location='dashboard.php';
-	</script>
-	<?php
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Sehat</title>
-    <?php
-		include "inner_links.php";
-		?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
 
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="../assets/images/icons/favicon.png"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/vendor/animate/animate.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/css/util.css">
-	<link rel="stylesheet" type="text/css" href="../assets/css/main.css">
-<!--===============================================================================================-->
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="../login/fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="../login/css/style.css">
 </head>
-<body style="background-color:  white;">
-<header class="main_menu">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-12">
-                        <nav class="navbar navbar-expand-lg navbar-light">
-                            <a class="navbar-brand" href="index.php"> <img src="../assets/img/logo.png" alt="logo"> </a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
+<body>
 
-                            <div class="collapse navbar-collapse main-menu-item justify-content-end"
-                                id="navbarSupportedContent">
-                                <ul class="navbar-nav align-items-center">
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="index.php">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="../index.php#contact">Contact</a>
-                                    </li>
-                                    <li class="d-none d-lg-block">
-                                        <a class="btn_1" href="https://www.mohfw.gov.in/">COVID 19</a>
-                                    </li>
-                                </ul>
+    <div class="main">
+        <!-- Sing in  Form -->
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="../login/images/signin-image.jpg" alt="sing up image"></figure>
+
+                    </div>
+
+                    <div class="signin-form">
+                        <h2 class="form-title">HOSPITAL SIGN IN</h2>
+                        <form method="POST" class="register-form" id="login-form" action="icu/functionality/iculoginact.php">
+                            <div class="form-group">
+                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="username" id="username" placeholder="User Name"/>
                             </div>
-                        </nav>
+                            <div class="form-group">
+                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Password"/>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </header>
+        </section>
 
-<!-- copy till here -->
+    </div>
 
 
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="login100-more" style="background-image: url('../assets/images/bg-04.jpg');"></div>
-			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-				<form class="login100-form validate-form" action="functionality/hospital_loginact.php" method="POST">
-					<span class="login100-form-title p-b-59">
-						Hospital Sign In
-					</span>
-					<div class="wrap-input100 validate-input" data-validate="Username is required">
-						<span class="label-input100">Username</span>
-						<input class="input100" type="text" name="username" placeholder="Username...">
-						<span class="focus-input100"></span>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="password" placeholder="*************">
-						<span class="focus-input100"></span>
-					</div>
-					<div class="container-login100-form-btn">
-						<div class="wrap-login100-form-btn">
-							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn">
-								Sign In
-							</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-
-<!--===============================================================================================-->
-	<script src="../assets/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/vendor/bootstrap/js/popper.js"></script>
-	<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/vendor/daterangepicker/moment.min.js"></script>
-	<script src="../assets/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/js/main.js"></script>
-    <?php include "../assets/footer.php"; ?>
-</body>
+    <!-- JS -->
+    <script src="../login/vendor/jquery/jquery.min.js"></script>
+    <script src="../login/js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
