136205240097259dbb51c44f4d96d3b4c82847e3
unknown <sarkaruttaran@gmail.com>
2020-09-17 01:02:42 +0530

    deleting unnecessary nurse files

--- nurse/functionality/ondutydoctoract.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../../assets/fxn.php";
-$_SESSION['department'] = $_POST["department"];
-$_SESSION['doctor'] = $_POST["doctor"];
-?>
-			<script type="text/javascript">
-        alert("On-Duty Doctor Set");
-        window.location = '../index.php';
-			</script>
