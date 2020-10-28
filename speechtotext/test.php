201f88ff390333ace6a8660753aec85dbbbcfdab
Tarun Kumar Dixit <tarundixit0310@gmail.com>
2020-10-28 22:38:55 +0530

    Some changes

--- speechtotext/test.php
+++ speechtotext/test.php
@@ -1,6 +1,18 @@
-<?php
-// $command = escapeshellcmd('python3 speechtotext/stt.py');
-// exec($command, $output, $return_var);
+<!DOCTYPE html>
+<html lang="en">
 
-$output = shell_exec('python3 /opt/lampp/htdocs/speechtotext/stt.py'); 
-echo $output;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <input type="file" accept="audio/*;capture=microphone">
+    <?php
+    $output = shell_exec('python3 /opt/lampp/htdocs/speechtotext/stt.py');
+    echo $output;
+    ?>
+</body>
+
+</html>
